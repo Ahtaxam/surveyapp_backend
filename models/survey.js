91b4428e5fe80ab2fbb14b57c8bf16ec104e1256
Ahtaxam <64573767+Ahtaxam@users.noreply.github.com>
2022-08-23 14:17:27 +0500

    feat: survey Schema

---
+++ models/survey.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const surveySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  questions: {
+    type: [
+      {
+        title: {
+          type: String,
+          required: true,
+        },
+        type: {
+          type: String,
+          required: true,
+          enum: ["text", "number", "checkbox", "multiplechoice"],
+        },
+        options: {
+          type: [String],
+        },
+      },
+    ],
+  },
+});
+
+const survey = mongoose.model("Survey", surveySchema);
+module.exports = survey;
