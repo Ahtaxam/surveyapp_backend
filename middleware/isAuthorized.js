3fe12aa2c1d118588979530bd390d0efd348f905
Ahtaxam <64573767+Ahtaxam@users.noreply.github.com>
2022-08-24 18:35:49 +0500

    refactor: made changes

--- middleware/isAuthorized.js
+++ middleware/isAuthorized.js
@@ -2,7 +2,7 @@ const Survey = require("../models/survey");
 
 async function isAuthorized(req, res, next) {
   const survey = await Survey.findById(req.params.id);
-  if (survey.userId !== req.user._id) {
+  if (survey.userId != req.user._id) {
     return res.status(401).json({ message: "Unauthorized" });
   }
   next();
