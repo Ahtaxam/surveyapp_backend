b534bdff3d03e962971058a002218f1ad655cf4b
Ahtaxam <64573767+Ahtaxam@users.noreply.github.com>
2022-08-31 15:39:07 +0500

    refactor: rename function

--- router/joinSurvey.js
+++ router/joinSurvey.js
@@ -2,7 +2,7 @@ const express = require("express");
 const route = express.Router();
 const joinSurvey = require("../controller/joinSurvey");
 
-route.get("/:id", joinSurvey.getJoinSurvey);
+route.get("/:id", joinSurvey.validateSurveyAccess);
 route.post("/", joinSurvey.submitResponse);
 
 module.exports = route;
