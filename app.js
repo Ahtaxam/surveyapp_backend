90435b32cedf8f37a4484a1ab2543c6fd19be2e1
Ahtaxam <64573767+Ahtaxam@users.noreply.github.com>
2022-09-05 21:09:12 +0500

    feat: add responses end point

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const loginuUser = require("./router/Auth");
 const createSurvey = require("./router/survey");
 const joinSurvey = require("./router/joinSurvey");
 const showOtherSurveys = require("./router/otherSurveys");
+const surveyResponses = require("./router/surveyResponses");
 const port = process.env.PORT || 8000;
 const url = process.env.DBURL;
 const cors = require("cors");
@@ -35,6 +36,7 @@ app.use("/login", loginuUser);
 app.use("/survey", isAuth, createSurvey);
 app.use("/join", isAuth, joinSurvey);
 app.use("/otherSurveys", isAuth, showOtherSurveys);
+app.use("/responses", isAuth, surveyResponses);
 app.listen(port, () => {
   console.log(`Server is listning on port ${port}`);
 });
