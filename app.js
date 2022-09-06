8d8bfcc254a0ba144b2d20690e04d420cbd1fa4e
Ahtaxam <64573767+Ahtaxam@users.noreply.github.com>
2022-09-06 19:36:15 +0500

    feat: add end point to download file

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const createSurvey = require("./router/survey");
 const joinSurvey = require("./router/joinSurvey");
 const showOtherSurveys = require("./router/otherSurveys");
 const surveyResponses = require("./router/surveyResponses");
+const download = require("./router/report");
 const port = process.env.PORT || 8000;
 const url = process.env.DBURL;
 const cors = require("cors");
@@ -37,6 +38,7 @@ app.use("/survey", isAuth, createSurvey);
 app.use("/join", isAuth, joinSurvey);
 app.use("/otherSurveys", isAuth, showOtherSurveys);
 app.use("/responses", isAuth, surveyResponses);
+app.use("/download", download);
 app.listen(port, () => {
   console.log(`Server is listning on port ${port}`);
 });
