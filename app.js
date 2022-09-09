f08410fd24fb28a1d72d5417bfeb46f1074bf376
Ahtaxam <64573767+Ahtaxam@users.noreply.github.com>
2022-09-09 18:58:37 +0500

    feat: add production route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
+const path = require("path");
 const app = express();
 
 const signUpUsers = require("./router/user");
@@ -44,3 +45,12 @@ app.use("/responsedetail", responseDetail);
 app.listen(port, () => {
   console.log(`Server is listning on port ${port}`);
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "surveyapp_frontend", "build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "surveyapp_frontend", "build", "index.html")
+    );
+  });
+}
